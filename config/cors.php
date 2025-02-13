31e87fa6c0d1365dbc3fb83d1249b87fac663225
Shakhawat Hossain <shakhawat.sk@gmail.com>
2025-02-13 11:03:02 +0600

    Cros update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000','https://ag-ecom.deviceinfotech.com')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'),'https://ag-ecom.deviceinfotech.com'],
 
     'allowed_origins_patterns' => [],
 
